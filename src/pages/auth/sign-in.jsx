d9de8d9a630eef6bef018426336a291b98c2e142
kaydist <kayodeoseni@ymail.com>
2022-12-19 14:21:45 +0100

    fix: removed transactions page

--- src/pages/auth/sign-in.jsx
+++ src/pages/auth/sign-in.jsx
@@ -25,7 +25,7 @@ export default function SignIn() {
   };
 
   if (isLoginingStatus === 'success') {
-    navigate(Routes.transactions)
+    navigate(Routes.users)
   }
 
   const formik = useFormik({
--- src/routes/Routes.js
+++ src/routes/Routes.js
@@ -2,7 +2,6 @@ export const Routes = {
   signup: "/sign-up",
   signin: "/",
   dashboard: "/dashboard",
-  transactions: "/transactions",
   agents: "/agents",
   users: "/users",
 };
